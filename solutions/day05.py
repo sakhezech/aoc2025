e049dbe000ef13bbc2d11c0f17dd13fef5901280
sakhezech <contact@sakhezech.ru>
2025-12-05 10:32:03 +0300

    feat: day05

---
+++ solutions/day05.py
@@ -0,0 +1,54 @@
+def parse(input: str) -> tuple[list[tuple[int, int]], list[int]]:
+    ranges_str, _, available_str = input.partition('\n\n')
+
+    ranges: list[tuple[int, int]] = []
+    for range_str in ranges_str.splitlines():
+        ll, _, rr = range_str.partition('-')
+        ranges.append((int(ll), int(rr)))
+    available = [int(v) for v in available_str.splitlines()]
+
+    return ranges, available
+
+
+def solve_part1(input: str) -> int:
+    ranges, available = parse(input)
+    c = 0
+    for id in available:
+        for ll, rr in ranges:
+            if ll <= id <= rr:
+                c += 1
+                break
+
+    return c
+
+
+def intersect(
+    r1: tuple[int, int], r2: tuple[int, int]
+) -> tuple[int, int] | None:
+    r1_left, r1_right = r1
+    r2_left, r2_right = r2
+    if any(r1_left <= r <= r1_right for r in r2):
+        return min(r1_left, r2_left), max(r1_right, r2_right)
+    return None
+
+
+def solve_part2(input: str) -> int:
+    ranges, _ = parse(input)
+
+    changed = True
+    while changed:
+        changed = False
+        for i, r1 in enumerate(ranges):
+            for j, r2 in enumerate(ranges):
+                if i == j:
+                    continue
+                intersected = intersect(r1, r2)
+                if intersected:
+                    ranges[i] = intersected
+                    del ranges[j]
+                    changed = True
+                    break
+            if changed:
+                break
+
+    return sum(rr - ll + 1 for ll, rr in ranges)
