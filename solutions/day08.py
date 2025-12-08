154b3f48a324b8b3ddc82a672a223480ee0bbaea
sakhezech <contact@sakhezech.ru>
2025-12-08 12:02:33 +0300

    feat: day08

---
+++ solutions/day08.py
@@ -0,0 +1,120 @@
+import dataclasses
+import math
+from typing import Self
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class v3:
+    x: int
+    y: int
+    z: int
+
+    def __add__(self, other: Self) -> Self:
+        return self.__class__(
+            self.x + other.x, self.y + other.y, self.z + other.z
+        )
+
+    def __sub__(self, other: Self) -> Self:
+        return self.__class__(
+            self.x - other.x, self.y - other.y, self.z - other.z
+        )
+
+    def __pow__(self, num: int | float) -> Self:
+        return self.__class__(self.x**num, self.y**num, self.z**num)
+
+    def get_distance(self, other: Self) -> float:
+        v = (self - other) ** 2
+        # NOTE: because we only need the distance for sorting
+        # we dont actually need to math.sqrt
+        # x > y -> math.sqrt(x) > math.sqrt(y)
+        return v.x + v.y + v.z  # return math.sqrt(v.x + v.y + v.z)
+
+
+def parse(input: str) -> list[v3]:
+    return [v3(*map(int, row.split(','))) for row in input.splitlines()]
+
+
+def solve_part1(input: str) -> int:
+    junctions = parse(input)
+    distances: list[tuple[float, v3, v3]] = []
+    for i, j1 in enumerate(junctions):
+        for j2 in junctions[i + 1 :]:
+            distances.append((j1.get_distance(j2), j1, j2))
+    distances.sort()
+
+    circuits: list[set[v3]] = []
+    cache: dict[v3, set[v3]] = {}
+
+    for _ in range(1000):
+        _, j1, j2 = distances.pop(0)
+        if j1 in cache and j2 in cache:
+            s1 = cache[j1]
+            s2 = cache[j2]
+            if s1 is s2:
+                continue
+
+            s1.update(s2)
+            circuits.remove(s2)
+            for j in s2:
+                cache[j] = s1
+        elif j1 in cache:
+            s = cache[j1]
+            s.add(j2)
+            cache[j2] = s
+        elif j2 in cache:
+            s = cache[j2]
+            s.add(j1)
+            cache[j1] = s
+        else:
+            s: set[v3] = set()
+            s.add(j1)
+            s.add(j2)
+            cache[j1] = s
+            cache[j2] = s
+            circuits.append(s)
+
+    return math.prod(sorted(len(s) for s in circuits)[-3:])
+
+
+def solve_part2(input: str) -> int:
+    junctions = parse(input)
+    distances: list[tuple[float, v3, v3]] = []
+    for i, j1 in enumerate(junctions):
+        for j2 in junctions[i + 1 :]:
+            distances.append((j1.get_distance(j2), j1, j2))
+    distances.sort()
+
+    circuits: list[set[v3]] = []
+    cache: dict[v3, set[v3]] = {}
+
+    result = 0
+    while distances:
+        _, j1, j2 = distances.pop(0)
+        if j1 in cache and j2 in cache:
+            s1 = cache[j1]
+            s2 = cache[j2]
+            if s1 is s2:
+                continue
+
+            s1.update(s2)
+            circuits.remove(s2)
+            for j in s2:
+                cache[j] = s1
+        elif j1 in cache:
+            s = cache[j1]
+            s.add(j2)
+            cache[j2] = s
+        elif j2 in cache:
+            s = cache[j2]
+            s.add(j1)
+            cache[j1] = s
+        else:
+            s: set[v3] = set()
+            s.add(j1)
+            s.add(j2)
+            cache[j1] = s
+            cache[j2] = s
+            circuits.append(s)
+        result = j1.x * j2.x
+
+    return result
