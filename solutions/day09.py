e5046464bdb7fc09e01ffebde18af0fef129e072
sakhezech <contact@sakhezech.ru>
2025-12-09 18:16:00 +0300

    feat: day09

---
+++ solutions/day09.py
@@ -0,0 +1,73 @@
+import dataclasses
+import itertools
+from typing import Self
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class v2:
+    x: int
+    y: int
+
+    def __add__(self, other: Self) -> Self:
+        return self.__class__(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Self) -> Self:
+        return self.__class__(self.x - other.x, self.y - other.y)
+
+
+def solve_part1(input: str) -> int:
+    red_tiles = [v2(*map(int, row.split(','))) for row in input.splitlines()]
+    return max(
+        (abs((rect := (a - b)).x) + 1) * (abs(rect.y) + 1)
+        for a, b in itertools.combinations(red_tiles, r=2)
+    )
+
+
+def range_in_rect(a: v2, b: v2, edge_tiles: set[v2]) -> bool:
+    # NOTE: from the future
+    # there has to be a better way to check this
+    # instead of going through each edge tile
+    # but i have spent too much time already
+    # maybe later
+    minx, maxx = sorted((a.x, b.x))
+    miny, maxy = sorted((a.y, b.y))
+    return any(minx < t.x < maxx and miny < t.y < maxy for t in edge_tiles)
+
+
+def solve_part2(input: str) -> int:
+    red_tiles = set(
+        v2(*map(int, row.split(','))) for row in input.splitlines()
+    )
+
+    path: list[v2] = []
+    start = min(red_tiles, key=lambda t: (t.y, t.x))
+    horiz = True
+
+    pos = start
+    while True:
+        possible_next = [
+            t
+            for t in red_tiles
+            if ((t.y == pos.y) if horiz else (t.x == pos.x)) and t is not pos
+        ]
+        assert len(possible_next) == 1
+
+        if len(path) and pos is start:
+            break
+        path.append(pos)
+
+        pos = possible_next[0]
+        horiz = not horiz
+
+    edge_tiles: set[v2] = set()
+    for ab in itertools.pairwise(path + [start]):
+        a, b = sorted(ab, key=lambda t: (t.y, t.x))
+        edge_tiles.update(
+            v2(x, y) for x in range(a.x, b.x + 1) for y in range(a.y, b.y + 1)
+        )
+
+    return max(
+        (abs((rect := (a - b)).x) + 1) * (abs(rect.y) + 1)
+        for a, b in itertools.combinations(red_tiles, r=2)
+        if not range_in_rect(a, b, edge_tiles)
+    )
